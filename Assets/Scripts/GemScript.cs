781d07c1dab255d97a170817a6a8f848214bde84
Ivas Ciprian <ciprianivas@yahoo.ro>
2025-02-03 12:40:49 +0200

    Add book that requires gems to open

---
+++ Assets/Scripts/GemScript.cs
@@ -0,0 +1,78 @@
+using NUnit.Framework.Constraints;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class GemScript : MonoBehaviour
+{
+    private bool _wasGrabbed;
+    private bool _wasPlacedInBook;
+    [SerializeField] private Transform _initialGemSlot;
+    [SerializeField] private Transform _bookGemSlot;
+    private XRGrabInteractable grabInteractable;
+    private bool isGrabbed = false;
+    [SerializeField] private Rigidbody _rigidbody;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.AddListener(OnGrab);
+            grabInteractable.selectExited.AddListener(OnRelease);
+        }
+    }
+
+    private void Update()
+    {
+        if (_wasPlacedInBook)
+        {
+            transform.position = _bookGemSlot.position;
+            transform.rotation = _bookGemSlot.rotation;
+            _rigidbody.isKinematic = true;
+            return;
+        }
+
+        if (_wasGrabbed)
+        {
+            return;
+        }
+
+        transform.position = _initialGemSlot.position;
+        transform.rotation = _initialGemSlot.rotation;
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        isGrabbed = true;
+        _wasGrabbed = true;
+        transform.parent = null;
+        Debug.Log(gameObject.name + " was grabbed!");
+    }
+
+    private void OnRelease(SelectExitEventArgs args)
+    {
+        isGrabbed = false;
+        _rigidbody.isKinematic = false;
+        Debug.Log(gameObject.name + " was released!");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Gem Slot")
+        {
+            _rigidbody.isKinematic = true;
+            GetComponent<SphereCollider>().enabled = false;
+            transform.position = other.transform.position;
+            _bookGemSlot = other.transform;
+            other.GetComponent<SphereCollider>().enabled = false;
+            _wasPlacedInBook = true;
+        }
+    }
+
+    public bool IsObjectGrabbed()
+    {
+        return isGrabbed;
+    }
+}
